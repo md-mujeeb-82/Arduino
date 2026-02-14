cdf146814329048a9f0b9fa3192eae3db5aa10c6
Mujeeb Mohammad <mohammad.mujeeb@gmail.com>
2026-02-15 00:56:22 +0530

    128x32 display to 128x64
    1. 128x32 display to 128x64.
    2. WiFi auto-off after 2 minutes.

--- ESP32/meditation.ino
+++ ESP32/meditation.ino
@@ -4,13 +4,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <WiFi.h>
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
 #include <WebServer.h>
 #include <ArduinoOTA.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 #ifndef STASSID
 #define STASSID "Meditation"
@@ -26,12 +24,15 @@ WebServer server(80);
 #define DEFAULT_MARKER 7867
 #define DEFAULT_FREQUENCY 13.0
 #define BLINK_DELAY 100000
+#define AP_TIMEOUT 120000000  // Microseconds
 long lastTime = -1;
 bool isPolirityForward = true;
 long halfCycleDelay = ((1/DEFAULT_FREQUENCY) * 1000 * 1000)/2;
 bool isPowerAC = true;
 bool blink = true;
 long lastTimeBlink;
+long wifiOnTime = -1;
+bool isWiFiOn = false;
 
 int PIN_SDA = 21;
 int PIN_SCL = 22;
@@ -47,6 +48,7 @@ struct Data {
 Data data;
 
 void handleRoot() {
+  wifiOnTime = micros();
   String html = R"=====(
     <!DOCTYPE html>
     <html>
@@ -71,6 +73,7 @@ void handleRoot() {
 }
 
 void handleSubmit() {
+  wifiOnTime = micros();
   String receivedValue = server.arg("frequency");
   float freq = receivedValue.toFloat();
   if(freq < 0 || freq > 5000) {
@@ -91,6 +94,7 @@ void handleSubmit() {
 }
 
 void handleNotFound() {
+  wifiOnTime = micros();
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -119,45 +123,46 @@ void updateDisplay() {
 
   // Not all the characters will fit on the display. This is normal.
   // Library will draw what it can and the rest will be clipped.
-  display.setCursor(36, 0);     // Start at top-left corner
+  display.setTextSize(2);       // Normal 1:1 pixel scale
+  display.setCursor(5, 5);     // Start at top-left corner
   display.write("MEDITATION");
 
-  display.setCursor(0, 10);
+  display.setTextSize(1);       // Normal 1:1 pixel scale
   if(isPowerAC) {
-    display.write("TYPE: AC");
+    display.setCursor(0, 30);
+    display.write("CURRENT TYPE: AC");
+    display.setCursor(0, 42);
+    display.write("VOLTAGE: 14.03 V");
   } else {
-    display.write("TYPE: DC");
+    display.setCursor(0, 30);
+    display.write("CURRENT TYPE: DC");
+    display.setCursor(0, 42);
+    display.write("VOLTAGE: 14.08 V");
   }
 
-  display.setCursor(0, 22);     // Start at top-left corner
-  char buffer[14];
-
-  if(isPowerAC) {
-    String str = "FREQ: " + String(data.frequency, 0) + " Hz";
-    str.toCharArray(buffer, 14);
-  } else {
-    String str = "VOLTAGE: 9 V";
-    str.toCharArray(buffer, 14);
-  }
+  display.setCursor(0, 54);     // Start at top-left corner
+  char buffer[128];
+  String str = "FREQUENCY: " + String(data.frequency, 0) + " Hz";
+  str.toCharArray(buffer, 128);
   display.write(buffer);
 
   display.display();
 }
 
 void animatePower() {
-  display.setCursor(55, 10);
+  display.setCursor(100, 42);
   display.setTextColor(SSD1306_BLACK);
  
   if(isPowerAC) {
     if(blink) {
       display.write("\\/\\/");
-      display.setCursor(55, 10);
+      display.setCursor(100, 42);
       display.setTextColor(SSD1306_WHITE);
       display.write("/\\/\\");
       blink = false;
     } else {
       display.write("/\\/\\");
-      display.setCursor(55, 10);
+      display.setCursor(100, 42);
       display.setTextColor(SSD1306_WHITE);
       display.write("\\/\\/");
       blink = true;
@@ -165,13 +170,13 @@ void animatePower() {
   } else {
     if(blink) {
       display.write("- -");
-      display.setCursor(55, 10);
+      display.setCursor(100, 42);
       display.setTextColor(SSD1306_WHITE);
       display.write(" - -");
       blink = false;
     } else {
       display.write(" - -");
-      display.setCursor(55, 10);
+      display.setCursor(100, 42);
       display.setTextColor(SSD1306_WHITE);
       display.write("- -");
       blink = false;
@@ -180,6 +185,56 @@ void animatePower() {
   }
 
   display.display();
+}
+
+void turnOnAP(bool isOn) {
+    if(isOn) {
+      // AP Mode
+      WiFi.softAP(STASSID, STAPSK);
+
+      // Port defaults to 8266
+      // ArduinoOTA.setPort(8266);
+
+      // Hostname defaults to esp8266-[ChipID]
+      ArduinoOTA.setHostname("Meditation");
+
+      // No authentication by default
+      // ArduinoOTA.setPassword("admin");
+
+      // Password can be set with it's md5 value as well
+      // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+      // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+      ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+          type = "sketch";
+        } else {  // U_FS
+          type = "filesystem";
+        }
+      });
+      
+      ArduinoOTA.begin();
+
+      // Web Server
+      server.on("/", handleRoot);
+      server.on("/submit", handleSubmit);
+      server.on("/inline", []() {
+        server.send(200, "text/plain", "this works as well");
+      });
+      server.onNotFound(handleNotFound);
+
+      server.begin();
+      isWiFiOn = true;
+      wifiOnTime = millis();
+    } else {
+
+      // MDNS.end();
+      ArduinoOTA.end();
+      server.stop();
+      WiFi.mode(WIFI_OFF);
+      isWiFiOn = false;
+    }
 }
 
 // the setup function runs once when you press reset or power the board
@@ -209,94 +264,33 @@ void setup() {
   Wire.begin(PIN_SDA, PIN_SCL);
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   
-  display.setTextSize(1);      // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
   updateDisplay();
 
-  // AP Mode
-  WiFi.softAP(STASSID, STAPSK);
-
-  // Station Mode
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin("Ahmads", "03312018");
-  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-  //   Serial.println("Connection Failed! Rebooting...");
-  //   delay(5000);
-  //   ESP.restart();
-  // }
-
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname("Meditation");
-  MDNS.begin("Meditation");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_FS
-      type = "filesystem";
-    }
-  });
-  
-  ArduinoOTA.begin();
-
-  // Web Server
-  server.on("/", handleRoot);
-
-  server.on("/submit", handleSubmit);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  // server.on("/gif", []() {
-  //   static const uint8_t gif[] PROGMEM = {
-  //     0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
-  //     0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
-  //     0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
-  //     0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
-  //     0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b
-  //   };
-  //   char gif_colored[sizeof(gif)];
-  //   memcpy_P(gif_colored, gif, sizeof(gif));
-  //   // Set the background to a random set of colors
-  //   gif_colored[16] = millis() % 256;
-  //   gif_colored[17] = millis() % 256;
-  //   gif_colored[18] = millis() % 256;
-  //   server.send(200, "image/gif", gif_colored, sizeof(gif_colored));
-  // });
-
-  server.onNotFound(handleNotFound);
-
-  MDNS.addService("http", "udp", 5353);
-  server.begin();
-
   // Initial setup
   digitalWrite(PIN_1, HIGH);
   digitalWrite(PIN_2, LOW);
+
+  turnOnAP(true);
 
   lastTime = lastTimeBlink = micros();
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  ArduinoOTA.handle();
-  server.handleClient();
-  // MDNS.update();
+
+  if(isWiFiOn) {
+    ArduinoOTA.handle();
+    server.handleClient();
+  }
 
   long currentTime = micros();
+
+  if(isWiFiOn && currentTime - wifiOnTime > AP_TIMEOUT) {
+    turnOnAP(false);
+  }
 
   if(currentTime - lastTimeBlink > BLINK_DELAY) {
     animatePower();
