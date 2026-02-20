abe63a9b794065d1adbb8aadff6f49760439ea18
Mujeeb Mohammad <mohammad.mujeeb@gmail.com>
2026-02-21 01:29:16 +0530

    Two-Way Communication.

--- ESP8266/SerialCommunication.ino
+++ ESP8266/SerialCommunication.ino
@@ -1,15 +1,26 @@
-//int pushButton = 2;
+int pushButton = 0;
+int vibrator = LED_BUILTIN;
 
 void setup() {
   Serial.begin(115200);
-  //pinMode(pushButton, INPUT);
+  pinMode(pushButton, INPUT_PULLUP);
+  pinMode(vibrator, OUTPUT);
+  digitalWrite(vibrator, HIGH);
 }
 
 void loop() {
-  if(Serial.available() <= 0) {
-    return;
+  if(digitalRead(pushButton) == 0) {
+    Serial.print("TICK");
+    delay(200);
   }
 
-  String value = Serial.readString();
-  Serial.print(value);
+  if(Serial.available()) {
+    String value = Serial.readString();
+    int val = value.toInt();
+    if(val == 0) {
+      digitalWrite(vibrator, LOW);
+      delay(100);
+      digitalWrite(vibrator, HIGH);
+    }
+  }
 }
